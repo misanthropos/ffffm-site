c7285a6752ab1bcdf27fc7086bec4b5507f76f33
skorpy <magnus.fruehling@gec.io>
2021-01-17 16:14:14 +0100

    update genpkglist.py

--- contrib/genpkglist.py
+++ contrib/genpkglist.py
@@ -157,7 +157,7 @@ PKGS_USB_NET = PackageList('USB_NET', [
 ])
 pkglists.append(PKGS_USB_NET)
 
-PKGS_PCI = PackageList('USB', ['pciutils'])
+PKGS_PCI = PackageList('PCI', ['pciutils'])
 pkglists.append(PKGS_PCI)
 
 PKGS_PCI_NET = PackageList('PCI_NET', [
@@ -167,7 +167,7 @@ pkglists.append(PKGS_PCI_NET)
 
 PKGS_TLS = PackageList('TLS', [
     'ca-bundle',
-    'libustream-mbedtls'
+    'libustream-opensl'
 ])
 pkglists.append(PKGS_TLS)
 
@@ -182,9 +182,6 @@ targets.get('ar71xx-generic'). \
     add_pkglist(PKGS_USB_STORAGE). \
     add_pkglist(PKGS_TLS). \
     exclude([  # devices without usb ports
-        'alfa-network-hornet-ub',
-        'alfa-network-tube2h',
-        'alfa-network-n2-n5',
         'allnet-all0315n',
         'avm-fritz-wlan-repeater-300e',
         'avm-fritz-wlan-repeater-450e',
@@ -199,15 +196,16 @@ targets.get('ar71xx-generic'). \
         'openmesh-om2p',
         'openmesh-om5p',
         'openmesh-om5p-ac',
-        'tp-link-cpe210-v1.0',
-        'tp-link-cpe210-v2.0',
-        'tp-link-cpe210-v3.0',
-        'tp-link-cpe510-v1.0',
-        'tp-link-wbs210-v1.20',
-        'tp-link-wbs510-v1.20',
+        'tp-link-cpe210-v1',
+        'tp-link-cpe210-v2',
+        'tp-link-cpe210-v3',
+        'tp-link-cpe510-v1',
+        'tp-link-wbs210-v1',
+        'tp-link-wbs510-v1',
         'tp-link-archer-c25-v1',
         'tp-link-archer-c58-v1',
         'tp-link-archer-c60-v1',
+        'tp-link-archer-c60-v2',
         'tp-link-re355',
         'tp-link-re450',
         'ubiquiti-airgateway',
@@ -225,6 +223,7 @@ targets.get('ar71xx-generic'). \
         'ubiquiti-unifiap-outdoor+',
         'ubiquiti-ls-sr71',
         'ubiquiti-unifi-ac-lite',
+        'ubiquiti-unifi-ac-lr',
         'ubiquiti-unifi-ac-pro',
         'ubiquiti-unifi-ac-mesh'], pkglists=[PKGS_USB, PKGS_USB_NET, PKGS_USB_SERIAL, PKGS_USB_STORAGE]). \
     exclude([  # devices with less than 64M memory
@@ -236,7 +235,23 @@ targets.get('ar71xx-generic'). \
         'tp-link-tl-wr1043n-nd-v1',
         'ubiquiti-airrouter'])
 
-for target in ['ar71xx-nand', 'ipq40xx', 'ipq806x', 'mpc85xx-generic', 'mvebu-cortexa9', 'ramips-mt7620', 'sunxi-cortexa7']:
+targets.get('ath79-generic'). \
+    add_pkglist(PKGS_USB). \
+    add_pkglist(PKGS_USB_NET). \
+    add_pkglist(PKGS_USB_SERIAL). \
+    add_pkglist(PKGS_USB_STORAGE). \
+    add_pkglist(PKGS_TLS). \
+    exclude([  # devices without usb ports
+        'devolo-wifi-pro-1200e',
+        'devolo-wifi-pro-1200i',
+        'devolo-wifi-pro-1750c',
+        'devolo-wifi-pro-1750i',
+        'devolo-wifi-pro-1750x',
+        'ocedo-raccoon',
+        'tp-link-archer-c6-v2',
+        'tp-link-cpe220-v3'], pkglists=[PKGS_USB, PKGS_USB_NET, PKGS_USB_SERIAL, PKGS_USB_STORAGE])
+
+for target in ['ar71xx-nand', 'ipq40xx-generic', 'ipq806x-generic', 'lantiq-xway', 'lantiq-xrx200', 'mpc85xx-generic', 'mpc85xx-p1020', 'mvebu-cortexa9', 'ramips-mt7620', 'sunxi-cortexa7']:
     targets.get(target). \
         add_pkglist(PKGS_USB). \
         add_pkglist(PKGS_USB_NET). \
@@ -262,6 +277,7 @@ targets.get('ramips-mt7621'). \
     add_pkglist(PKGS_USB_STORAGE). \
     add_pkglist(PKGS_TLS). \
     exclude([  # devices without usb ports
+        'netgear-ex6150',
         'ubnt-erx',
         'ubnt-erx-sfp'], pkglists=[PKGS_USB, PKGS_USB_NET, PKGS_USB_SERIAL, PKGS_USB_STORAGE])
 
@@ -272,7 +288,11 @@ targets.get('ramips-mt76x8'). \
     add_pkglist(PKGS_USB_STORAGE). \
     add_pkglist(PKGS_TLS). \
     exclude([   # devices without usb ports
+        'cudy-wr1000',
+        'gl.inet-vixmini',
         'tp-link-archer-c50-v3',
+        'tp-link-archer-c50-v4',
+        'tp-link-tl-wa801nd-v5',
         'tp-link-tl-wr841n-v13'], pkglists=[PKGS_USB, PKGS_USB_NET, PKGS_USB_SERIAL, PKGS_USB_STORAGE])
 
 for target in ['x86-64', 'x86-generic', 'x86-geode']:
